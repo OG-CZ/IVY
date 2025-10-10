938ffd19241654f7d7560a2749033466d3e87bb8
og-cz <16796127+og-cz@users.noreply.github.com>
2025-10-10 21:23:32 +0800

    Remove old model files and changed to OPEN-AI Whisper

--- features.py
+++
@@ -1,9 +0,0 @@
-from playsound import playsound
-import eel
-
-
-# play assistant sound function
-@eel.expose
-def play_assistant_sound():
-    music_dir = "www\\assets\\audio\\start_sound.mp3"
-    playsound(music_dir)
