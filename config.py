8654c12781a54e57389b95b13efdf0ff935b2c5f
og-cz <16796127+og-cz@users.noreply.github.com>
2025-10-11 10:01:13 +0800

    refactored

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import time
+
+# assistant info
 ASSISTANT_NAME = "Ivy"
 MODEL_DIR = "machine-learning/faster-whisper-small.en"
 MODEL_BACKUP = "model/"
+
+# setup
+HOST = "localhost"
+PORT = 5500
+VERSION = int(time.time())
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import os
 import time
 import eel
-from lib.vocal.features import *
-from lib.vocal.command import *
+from lib.main.features import *
+from lib.main.command import *
 
-eel.init("www")
-
-HOST = "localhost"
-PORT = 5500
-VERSION = int(time.time())
+eel.init("ui")
 
 
 def main():
-    start_page = f"index.html?v={VERSION}"
-    os.system(f'start msedge.exe --app="http://{HOST}:{PORT}/{start_page}"')
+    start_page = f"index.html?v={config.VERSION}"
+    os.system(
+        f'start msedge.exe --app="http://{config.HOST}:{config.PORT}/{start_page}"'
+    )
 
     eel.start(
         start_page,
-        options={"mode": None, "host": HOST, "port": PORT, "chromeFlags": []},
+        options={
+            "mode": None,
+            "host": config.HOST,
+            "port": config.PORT,
+            "chromeFlags": [],
+        },
         block=True,
     )
 
