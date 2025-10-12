1268492b57116db4b0e24f65557a3eb5dc8a0ad2
og-cz <16796127+og-cz@users.noreply.github.com>
2025-10-12 23:16:38 +0800

    configuration files

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import time
+import api
 
 # assistant info
 ASSISTANT_NAME = "Ivy"
@@ -9,3 +10,7 @@ MODEL_BACKUP = "model/"
 HOST = "localhost"
 PORT = 5500
 VERSION = int(time.time())
+
+# hot word detection api -> https://console.picovoice.ai/ - sign up to get key
+PORCUPINE_API_KEY = api.PORCUPINE_API_KEY  # paste here
+PORCUPINE_IVY_HOTKEY_MODEL = "model/hot-word-detection/hey-ivy_en_windows_v3_0_0.ppn"
