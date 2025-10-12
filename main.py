b8a9fc33f8970e5de1370ff766b0e0e24391fc47
og-cz <16796127+og-cz@users.noreply.github.com>
2025-10-12 23:17:03 +0800

    Main application changes

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import eel
 from lib.main.features import *
 from lib.main.command import *
 
-eel.init("ui")
 
+def start():
+    eel.init("ui")
 
-def main():
     start_page = f"index.html?v={config.VERSION}"
     os.system(
         f'start msedge.exe --app="http://{config.HOST}:{config.PORT}/{start_page}"'
@@ -22,7 +22,3 @@ def main():
         },
         block=True,
     )
-
-
-if __name__ == "__main__":
-    main()
