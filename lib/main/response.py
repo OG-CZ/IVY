262d8fa1250526cac0b0bd7cfb3e443bb14ac7af
og-cz <16796127+og-cz@users.noreply.github.com>
2025-10-14 23:30:54 +0800

    add/update welcome-back lines for post-auth UX

--- lib/main/response.py
+++ lib/main/response.py
@@ -1,56 +1,63 @@
 # negative response for query that is not understandable
 cannot_understand_user = [
-    "Sorry, I didn't catch that.",
-    "I'm not sure if i understand.",
+    "Sorry, I didn't catch that",
+    "I'm not sure if i understand",
     "Could you rephrase that?",
-    "Hmm, I don't know about that.",
-    "I don't understand what you mean.",
+    "Hmm, I don't know about that",
+    "I don't understand what you mean",
     "Im sorry, can you clarify?",
     "I didn't get that. Can you say it again?",
-    "I’m not sure how to respond to that.",
-    "Sorry, I cannot process that request.",
+    "I’m not sure how to respond to that",
+    "Sorry, I cannot process that request",
     "I don’t quite get it. Can you try again?",
 ]
 
 # response when query is false in youtube search
 cannot_understand_youtube = [
-    "Sorry, I couldn't find that video on YouTube.",
-    "Hmm, I didn't understand which video to play.",
-    "I couldn't figure out what to play on YouTube.",
-    "Oops! That video doesn't seem to exist.",
-    "Sorry, I can't play that video right now.",
+    "Sorry, I couldn't find that video on YouTube",
+    "Hmm, I didn't understand which video to play",
+    "I couldn't figure out what to play on YouTube",
+    "Oops! That video doesn't seem to exist",
+    "Sorry, I can't play that video right now",
     "I didn't get the video title, can you try again?",
-    "Hmm, that request is unclear, please repeat.",
-    "I couldn't process your YouTube request.",
-    "That video seems unavailable or invalid.",
-    "Sorry, I don't know which video you mean.",
+    "Hmm, that request is unclear, please repeat",
+    "I couldn't process your YouTube request",
+    "That video seems unavailable or invalid",
+    "Sorry, I don't know which video you mean",
 ]
 
 cannot_find_app = [
-    "Sorry, I couldn't find {app} in my database. Please check if it's added to the system.",
+    "Sorry, I couldn't find {app} in my database. Please check if it's added to the system",
     "I don't recognize the app '{app}'",
-    "'{app}' is not available. Try another app or update my system commands.",
-    "I can't open '{app}' because it's not in my list.",
+    "'{app}' is not available. Try another app or update my system commands",
+    "I can't open '{app}' because it's not in my list",
 ]
 
 cannot_understand_app_name = [
     "Sorry, which app did you mean?",
     "Could you repeat the app name for me?",
-    "I didn’t quite catch the application you said.",
+    "I didn’t quite catch the application you said",
     "What app should I open?",
     "Mind saying the app name again?",
 ]
 
 cannot_find_whatsapp_contact = [
-    "Sorry, I couldn't find a contact in your message. Please try again.",
+    "Sorry, I couldn't find a contact in your message. Please try again",
     "I wasn't able to identify a contact. Could you repeat the name or number?",
-    "No contact found. Please specify who you want to message or call.",
-    "I need a contact name or number to proceed. Please try again.",
+    "No contact found. Please specify who you want to message or call",
+    "I need a contact name or number to proceed. Please try again",
 ]
 
 cannot_send_whatsapp_message = [
-    "Sorry, I couldn't send the WhatsApp message.",
-    "I was unable to deliver your WhatsApp message.",
-    "There was a problem sending your WhatsApp message.",
-    "I couldn't complete your WhatsApp request. Please try again.",
+    "Sorry, I couldn't send the WhatsApp message",
+    "I was unable to deliver your WhatsApp message",
+    "There was a problem sending your WhatsApp message",
+    "I couldn't complete your WhatsApp request. Please try again",
+]
+
+welcome_back_user = [
+    "Welcome back! Ivy is here to assist you 24/7 and 7 days a week",
+    "Good to see you again. How can I help you today?",
+    "Welcome back! Ready when you are",
+    "Hi again! Let's get started",
 ]
